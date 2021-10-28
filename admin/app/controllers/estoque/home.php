3f185bf70de4ba8185a580993c52d57c0f5bb147
gabriel-fox3 <61973325+gabriel-fox3@users.noreply.github.com>
2021-10-28 12:40:32 -0300

    front produtos

--- admin/app/controllers/estoque/home.php
+++ admin/app/controllers/estoque/home.php
@@ -6,11 +6,16 @@ class ControllerEstoqueHome extends BaseController {
   public function index() {
 
     $this->document->setTitle('Estoque');
+    $this->document->addStyle('css/styles/estoque');
+
+    $this->document->addStyle('js/plugins/iconpicker/css/fontawesome-iconpicker');
+    $this->document->addScript('js/plugins/iconpicker/js/fontawesome-iconpicker.min');
 
     $data['user_info'] = $this->session->data['user_info'];
     
-    $data['sidebar'] = $this->load->controller('common/sidebar');
-    $data['navbar'] = $this->load->controller('common/navbar');
+    $data['add'] = $this->load->view('estoque/add', $data);
+    $data['sidebar'] = $this->load->controller('common/sidebar', $data);
+    $data['navbar'] = $this->load->controller('common/navbar', $data);
     $data['header'] = $this->load->controller('common/header', $data);
     $data['footer'] = $this->load->controller('common/footer', $data);
 
