f42969d1358c85b2f86a1d14a2e28aaab00c326e
gabriel-fox3 <61973325+gabriel-fox3@users.noreply.github.com>
2022-01-05 17:21:04 -0300

    Update app.php

--- app/configs/app.php
+++ app/configs/app.php
@@ -3,21 +3,21 @@
 define('USE_TWIG', true);
 define('USE_SSL', false);
 
-define('HTTP_SERVER', 'http://localhost/live/divino/');
-define('HTTPS_SERVER', 'https://localhost/live/divino/');
+define('HTTP_SERVER', 'http://rep.fox3.com.br/divino/');
+define('HTTPS_SERVER', 'https://rep.fox3.com.br/divino/');
 // define('HTTP_SERVER', 'http://rep.fox3.com.br/divino/');
 // define('HTTPS_SERVER', 'https://rep.fox3.com.br/divino/');
 
 define('OLD_SERVER', 'http://beto.fox3.com.br/beto-v2/');
 
 if (USE_SSL == true) {
-  define('UPLOADS', 'http://localhost/live/divino/framework/storage/uploads/');
-  define('DEFAULT_IMG_USER', 'http://localhost/live/divino/assets/img/default-avatar.png');
-  define('DEFAULT_NO_PHOTO', 'http://localhost/live/divino/assets/img/no_photo.png');
+  define('UPLOADS', 'http://rep.fox3.com.br/divino/framework/storage/uploads/');
+  define('DEFAULT_IMG_USER', 'http://rep.fox3.com.br/divino/assets/img/default-avatar.png');
+  define('DEFAULT_NO_PHOTO', 'http://rep.fox3.com.br/divino/assets/img/no_photo.png');
 } else {
-  define('UPLOADS', 'http://localhost/live/divino/framework/storage/uploads/');
-  define('DEFAULT_IMG_USER', 'http://localhost/live/divino/assets/img/default-avatar.png');
-  define('DEFAULT_NO_PHOTO', 'http://localhost/live/divino/assets/img/no_photo.png');
+  define('UPLOADS', 'http://rep.fox3.com.br/divino/framework/storage/uploads/');
+  define('DEFAULT_IMG_USER', 'http://rep.fox3.com.br/divino/assets/img/default-avatar.png');
+  define('DEFAULT_NO_PHOTO', 'http://rep.fox3.com.br/divino/assets/img/no_photo.png');
 }
 
 // // DB Config
